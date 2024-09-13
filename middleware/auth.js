be30e96102c64538ee41aa7090107a6bb41782b3
Vasilis-Rousis <vasilisrousis@gmail.com>
2024-09-13 13:20:53 +0300

    Fixes to Authentication middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,9 @@
 // middleware/auth.js
-export default defineNuxtRouteMiddleware(async (to, from) => {
-  const supabase = useSupabaseClient();
-
-  // Get the current user
-  const { data: user, error } = await supabase.auth.getUser();
-
-  console.log("USERRR", user);
-
-  // List of public routes that do not require authentication
-  const publicPaths = ["/login", "/sign-up"];
-
-  if (error) { 
-    return navigateTo("/login");
-  }
+export default defineNuxtRouteMiddleware(async () => {
+  const user = useSupabaseUser();
 
   // If there's an error or no user, and they're trying to access a protected route, redirect to login
-  if (!user && !publicPaths.includes(to.path)) {
+  if (!user.value) {
     return navigateTo("/login");
   }
 });
